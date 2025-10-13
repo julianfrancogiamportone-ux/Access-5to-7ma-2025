99f57c601ad20c53a841305d989b5b259e107145
emmainsanopro2008-a11y <emmanuelplanas2@gmail.com>
2025-10-13 08:21:20 -0300

    Access
    Pagina de compra de videojuegos virtuales.
    Primera subida a la web.

---
+++ src/main/java/Acces/api/Acces/AccesApplication.java
@@ -0,0 +1,13 @@
+package Acces.api.Acces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Acces/api/Acces/AccesApplicationTests.java
@@ -0,0 +1,13 @@
+package Acces.api.Acces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
