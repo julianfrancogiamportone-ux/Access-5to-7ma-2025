cb87ac3e590e5d6b4c4c3b1a5e4ce2377527c3ff
Juan Cruz Suarez <sjuancruz24@gmail.com>
2025-10-26 19:45:13 -0300

    Trailer
    Creacion de la entidad Trailer

---
+++ src/main/java/Acces/api/Acces/entity/Trailer.java
@@ -0,0 +1,76 @@
+package Acces.api.Acces.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "trailers")
+public class Trailer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descripcion;
+
+    private String urlVideo; // por ejemplo, un link de YouTube o ruta al archivo mp4
+
+    @ManyToOne
+    @JoinColumn(name = "juego_id")
+    private Juego juego;
+
+    // Constructor vacío
+    public Trailer() {}
+
+    // Constructor con parámetros
+    public Trailer(String titulo, String descripcion, String urlVideo, Juego juego) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.urlVideo = urlVideo;
+        this.juego = juego;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrlVideo() {
+        return urlVideo;
+    }
+
+    public void setUrlVideo(String urlVideo) {
+        this.urlVideo = urlVideo;
+    }
+
+    public Juego getJuego() {
+        return juego;
+    }
+
+    public void setJuego(Juego juego) {
+        this.juego = juego;
+    }
+}
